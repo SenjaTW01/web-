3535e2b04b829e78eca67603baf001779723980a
SenjaTW01 <senjatwilight3@gmail.com>
2023-10-16 16:53:09 +0700

    first project class

---
+++ src/components/BoxOldWeb.jsx
@@ -0,0 +1,16 @@
+
+const BoxOldWeb = () => {
+	return (
+		<div id="BoxOldWeb">
+			<a href="https://www.tiktok.com/@spywaree?_t=8gT4zrrwz3w&_r=1">
+				<div className="flex justify-between">
+					<img src="/link.png" alt="" className="w-auto h-6" />
+					<img src="/next.png" alt="" className="h-3 w-3" />
+				</div>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">Tik Tok Kami</h1>
+			</a>
+		</div>
+	)
+}
+
+export default BoxOldWeb
