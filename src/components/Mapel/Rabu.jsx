109edc8decc18e653bd491f29097fd87eda2786c
SenjaTW01 <senjatwilight3@gmail.com>
2023-10-18 11:18:02 +0700

    jadwal kamis rebo

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -3,24 +3,19 @@ const Rabu = () => {
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
 					<div className="w-[50%] text-base  font-medium">KKRPL-11</div>
-					<div className="flex justify-center items-center text-sm">07.00-09.00</div>
+					<div className="flex justify-center items-center text-sm">Jam ke 1 - 4</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">09.00-09.15</div>
+					<div className="w-[50%] text-base  font-medium">PAI</div>
+					<div className="flex justify-center items-center text-sm">Jam ke 5 - 7</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">KKRPL-11</div>
-					<div className="flex justify-center items-center text-sm">09.15-09.55</div>
+					<div className="w-[50%] text-base  font-medium">Inggris</div>
+					<div className="flex justify-center items-center text-sm">Jam ke 8 - 9</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">PAI</div>
-					<div className="flex justify-center items-center text-sm">09.55-11.55</div>
-				</div>
-
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60 border-b-2 border-white" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">11.55-12.25</div>
+					<div className="w-[50%] text-base  font-medium">INDONESIA</div>
+					<div className="flex justify-center items-center text-sm">Jam ke 10 - 12</div>
 				</div>
 			
         </>
